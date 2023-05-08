129515fb4e7b3628dc33d208c5e38b4ed6cc1ff1
YavorVladev <boxmmox@gmail.com>
2023-05-08 23:50:55 +0300

    Palindrome Number
    The description of the problem is inside the ".py" file.

--- Math Problems/palindrome_number.py
+++ Math Problems/palindrome_number.py
@@ -4,3 +4,16 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
 
+                                      # Different Solution
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x < 0:
+#             return False
+#         rev = 0
+#         orig = x
+#         while x != 0:
+#             rev = rev * 10 + x % 10
+#             x //= 10
+#         return rev == orig
+
