49293586863fcff100c08dff0ffdf62a3ff793ec
YavorVladev <boxmmox@gmail.com>
2023-05-14 01:05:51 +0300

    Climbing Stairs
    The description of the problem is inside the ".py" file.

--- Math Problems/climbing_stairs.py
+++ Math Problems/climbing_stairs.py
@@ -33,6 +33,21 @@ class Solution:
 
         return climb(n)
 
+                                # Different Solution - Simplified
+
+#     def climbStairs(n: int) -> int:
+#     if n <= 2:
+#         return n
+#
+#     dp = [0] * (n + 1)
+#     dp[1] = 1
+#     dp[2] = 2
+#
+#     for i in range(3, n + 1):
+#         dp[i] = dp[i - 1] + dp[i - 2]
+#
+#     return dp[n]
+
 
 sol = Solution()
 number = 7
