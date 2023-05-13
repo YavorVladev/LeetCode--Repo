718f92a658b2aa83957981d3c8b6c9b0b717d7af
YavorVladev <boxmmox@gmail.com>
2023-05-13 23:53:36 +0300

    Climbing Stairs
    The description of this problem is inside the ".py" file.

---
+++ Math Problems/climbing_stairs.py
@@ -0,0 +1,40 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+#
+# Example 1:
+#
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+#
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        data = {}
+        data[1] = 1
+        data[2] = 2
+
+        def climb(n):
+            if n in data:
+                return data[n]
+            else:
+                data[n] = climb(n - 1) + climb(n - 2)
+                return data[n]
+
+        return climb(n)
+
+
+sol = Solution()
+number = 7
+result = sol.climbStairs(number)
+print(result)
