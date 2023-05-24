3134635192af64c0ea0cb99ae81945254e2fb440
YavorVladev <boxmmox@gmail.com>
2023-05-24 23:47:19 +0300

    Valid Parentheses
    The description of this problem is inside the ".py" file.

---
+++ String Problems/valid_parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        is_good = False
+
+        for i in range(len(s) - 1):
+            if s[i] == "(":
+                if s[i + 1] == ")":
+                    is_good = True
+                else:
+                    is_good = False
+
+            elif s[i] == "[":
+                if s[i + 1] == "]":
+                    is_good = True
+                else:
+                    is_good = False
+
+            elif s[i] == "{":
+                if s[i + 1] == "}":
+                    is_good = True
+                else:
+                    is_good = False
+
+        return is_good
+
+
+sol = Solution()
+text = "(){}}{"
+res = sol.isValid(text)
+print(res)
