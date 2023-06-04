a44b157ab12b58cabcbbc3a6573d607653a36a5f
YavorVladev <boxmmox@gmail.com>
2023-06-04 23:32:24 +0300

    Stock Prediction
    The description of this problem is inside the ".py" file.

---
+++ LeetCode Array Problems/stock_prediction.py
@@ -0,0 +1,23 @@
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you
+# must buy before you sell.
+
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        if not prices:
+            return 0
+
+        max_profit = 0
+        min_purchase = prices[0]
+        for i in range(1, len(prices)):
+            max_profit = max(max_profit, prices[i] - min_purchase)
+            min_purchase = min(min_purchase, prices[i])
+        return max_profit
+
+
+sol = Solution()
+stocks = [2, 4, 1]
+res = sol.maxProfit(stocks)
+print(res)
