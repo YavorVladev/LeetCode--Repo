3a5a0b450978078d52013ca8db0f01dbb9507aa9
YavorVladev <boxmmox@gmail.com>
2023-06-05 00:24:37 +0300

    Stock Prediction
    The description of this problem is in the ".py" file.

--- LeetCode Array Problems/stock_prediction.py
+++ LeetCode Array Problems/stock_prediction.py
@@ -17,6 +17,26 @@ class Solution:
         return max_profit
 
 
+# class Solution:
+#     def maxProfit(self, prices: list[int]) -> int:
+#         left = 0
+#         right = 1
+#
+#         max_profit = 0
+#
+#         while right < len(prices):
+#             current_profit = prices[right] - prices[left]
+#
+#             if prices[left] < prices[right]:
+#                 max_profit = max(current_profit, max_profit)
+#             else:
+#                 left = right
+#
+#             right += 1
+#
+#
+#         return max_profit
+
 sol = Solution()
 stocks = [2, 4, 1]
 res = sol.maxProfit(stocks)
