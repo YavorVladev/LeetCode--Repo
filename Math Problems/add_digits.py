427f36c34d9b5f6bcb8890102640b4511aa7d3fc
YavorVladev <boxmmox@gmail.com>
2023-07-29 23:43:28 +0300

    Add Digits
    The description of this problem is inside the ".py" file.

--- Math Problems/add_digits.py
+++ Math Problems/add_digits.py
@@ -1,3 +1,7 @@
+# Given an integer num, repeatedly add all its digits
+# until the result has only one digit, and return it.
+
+
 class Solution:
     def addDigits(self, num: int) -> int:
         if (num == 0): return 0
