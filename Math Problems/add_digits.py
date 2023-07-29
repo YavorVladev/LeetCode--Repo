8419b892e57ded542f306f7d47dd30d2f505b59b
YavorVladev <boxmmox@gmail.com>
2023-07-29 23:41:17 +0300

    Add Digits
    The description of this problem is inside the ".py" file.

---
+++ Math Problems/add_digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if (num == 0): return 0
+        if num % 9 == 0:
+            return 9
+        else:
+            return num % 9
+
+
+sol = Solution()
+n = 38
+res = sol.addDigits(n)
+print(res)
