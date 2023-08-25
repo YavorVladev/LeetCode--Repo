9c32a738d611a6fe5c5af762abf5f90ab6a770d9
YavorVladev <boxmmox@gmail.com>
2023-08-25 22:49:44 +0300

    Rotate Image
    The description of this problem is inside the ".py" file.

--- LeetCode Array Problems/rotate_image.py
+++ LeetCode Array Problems/rotate_image.py
@@ -29,4 +29,10 @@ class Solution:
             for j in range(i + 1, len(matrix[i])):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
+
 # Time Complexity O(n^2)
+
+sol = Solution()
+matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+res = sol.rotate(matrix)
+print(res)
