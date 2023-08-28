e3bf692ce8ffc0b76c2372fb010da9755276a927
YavorVladev <boxmmox@gmail.com>
2023-08-28 14:14:13 +0300

    Bit Plus Plus
    The description of this problem is inside the ".py" file.

---
+++ Math Problems/bit_plus_plus.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+total_score = 0
+
+for statement in range(n):
+    sequence = input()
+
+    plus = 0
+    minus = 0
+
+    for ch in sequence:
+        if ch == "+":
+            plus += 1
+
+            if plus >= 2:
+                total_score += 1
+
+
+        elif ch == "-":
+            minus += 1
+
+            if minus >= 2:
+                total_score -= 1
+
+print(total_score)
