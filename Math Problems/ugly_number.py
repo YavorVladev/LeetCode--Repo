cd19807aad320c6c0e70cf32a864a336fe4d983f
YavorVladev <boxmmox@gmail.com>
2023-09-25 23:39:07 +0300

    Ugly Number
    The description of this problem is inside the ".py" file.

---
+++ Math Problems/ugly_number.py
@@ -0,0 +1,26 @@
+# Example 1:
+#
+# Input: n = 6
+# Output: true
+# Explanation: 6 = 2 × 3
+
+# Example 2:
+#
+# Input: n = 1
+# Output: true
+# Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+
+# Example 3:
+#
+# Input: n = 14
+# Output: false
+# Explanation: 14 is not ugly since it includes the prime factor 7.
+#
+
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        if num == 0: return False
+        while num % 5 == 0: num /= 5
+        while num % 3 == 0: num /= 3
+        while num % 2 == 0: num /= 2
+        return num == 1
